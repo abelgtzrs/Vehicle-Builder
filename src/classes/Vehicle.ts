bd6ee65befed5660f924067152234fdaac77f45e
abelgtzrs <32408235+abelgtzrs@users.noreply.github.com>
2025-01-16 15:23:41 -0500

    Trucks and Vehicles updated

--- src/classes/Vehicle.ts
+++ src/classes/Vehicle.ts
@@ -4,21 +4,45 @@ import Driveable from '../interfaces/Driveable.js';
 // Vehicle class that implements Driveable interface
 class Vehicle implements Driveable {
   // Declare properties of the Vehicle class
+  vin: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
   started: boolean;
   currentSpeed: number;
 
   // Constructor for the Vehicle class
-  constructor() {
+  constructor(
+    vin: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number
+  ) {
+    this.vin = vin;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
     this.started = false;
     this.currentSpeed = 0;
   }
 
   // Method to print vehicle details
   printDetails(): void {
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight} lbs`);
+    console.log(`Top Speed: ${this.topSpeed} mph`);
     console.log(`Vehicle started: ${this.started}`);
-    console.log(`Vehicle current speed: ${this.currentSpeed} mph`);
+    console.log(`Current Speed: ${this.currentSpeed} mph`);
   }
-
   // Method to start the vehicle
   start(): void {
     this.started = true;
